bf747c9199cd92f286e052e8a821ce433ab0e93e
Juan Manuel Uribe Medina <juanmanueluribemedina@192.168.1.6>
2021-04-23 22:51:32 -0500

    Kata 05 version 2 con potencias

--- katas/kata-05-fibonacci-millonesim.js
+++ katas/kata-05-fibonacci-millonesim.js
@@ -1,19 +1,45 @@
-const fib = (n) => {
-    if(n <= 2)
-        return BigInt(1);
 
-    let a = 1n, b = 1n, c = 0n;
+const mmultiter = (fm, counter) => {
+    if(counter === 0){
+        return [
+            [1n,0n],
+            [0n,1n]
+        ];
+    };
 
-    for(i=3;i<=n;i++){
-        c = a + b;
-        [a,b] = [b,c];
+    if(counter % 2 === 0){
+        let fn = mmultiter(fm, counter / 2);
+        return mmult(fn,fn);
+    } else {
+        let fn = mmultiter(fm, counter - 1);
+        return mmult(fm,fn);
     }
+}
 
-    return c;
+const mmult = (fm, fn) => {
+    return [
+        [
+            ((fm[0][0] * fn[0][0]) + (fm[0][1] * fn[1][0])), 
+            ((fm[0][0] * fn[0][1]) + (fm[0][1] * fn[1][1]))
+        ],[
+            ((fm[1][0] * fn[0][0]) + (fm[1][1] * fn[1][0])),
+            ((fm[1][0] * fn[0][1]) + (fm[1][1] * fn[1][1]))
+        ]
+    ];
+}
+
+const fib = (n) => {
+    const f = [
+        [0n,1n],
+        [1n,1n]
+    ];
+    
+    let result =  mmultiter(f,Math.abs(n))[0][1];
+    return n < 0 && n % 2 === 0 ? result * -1n : result;
 };
 
-const n = 2000000;
 
-console.time('iterativo')
-console.log(fib(n));
-console.timeEnd('iterativo');
+console.time('potencias');
+console.log(fib(-20));
+console.timeEnd('potencias');
+
